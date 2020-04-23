803f4d0689e77cb756ff851b1d27cf8a23ee86a5
Selestine <ambeceles@gmail.com>
2020-04-23 14:08:54 +0400

    Update javasript.js

--- javasript.js
+++ javasript.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
     getAllCountries();
     $(document).on('click', '.store-container', function() {
         setMarkerData($(this).data('slug'));
+Const areaform = document.querySelector('#formArea');
+areaform.style.display = 'none';
+setTimeout(function (){
+areaform.style.display = 'block';
+},5000);
 
     });
 });
